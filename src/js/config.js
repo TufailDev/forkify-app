8ed5403ce762c18b9f24f4ebb5f5be13824e2f8e
Tufail <tufaildev24@gmail.com>
2021-02-17 19:41:31 +0530

    Added the key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'c094cef6-70b6-4f85-a7f5-6806b4fd05ef';
 export const MODAL_CLOSE_SEC = 2.5;
